e48e206fac703d70a02b7f2b63f3083fbb2efc47
Alexander SHZ <xxxshzxxx@gmail.com>
2020-08-12 16:42:51 +0300

    Добавьте файлы проекта.

---
+++ 11matrix.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int incomingData()
+
+
+int main()
+{
+    printf("%d" incomingData());
+}
+
+int incomingData()
+{
+    int string, column,
+
+        printf("Enter number of strings \n");
+    string = getchar();
+    printf("Enter number of columns \n");
+    column = getchar();
+
+    int matrix[string][column] = (int*)malloc(sizeof(int) * string * column);
+
+    printf("Enter numbers to the massive \n");
+    for (int i = 0; i < string; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            matrix[i][j] = getchar();
+        }
+    }
+
+    return matrix[string][column];
+}
